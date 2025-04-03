c9ac5d19eb4acc5681a9062396938ccbc7e90d04
dmani <dmani@statzcorp.com>
2025-04-03 12:31:45 -0500

    Update settings for improved environment configuration and security:
    - Enhanced `settings.py` to include additional environment variable checks, ensuring better configuration management.
    - Refined security settings by adjusting `SECURE_BROWSER_XSS_FILTER` and `SECURE_CONTENT_TYPE_NOSNIFF` for improved protection against XSS attacks.
    - Updated `requirements.txt` to include `python-decouple` for better handling of environment variables, promoting cleaner code and separation of concerns.

---
+++ STATZWeb/management/commands/runserver_custom.py
@@ -0,0 +1,9 @@
+from django.core.management.commands.runserver import Command as RunserverCommand
+import os
+from dotenv import load_dotenv
+
+class Command(RunserverCommand):
+    def run_from_argv(self, argv):
+        load_dotenv()
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE'))
+        super().run_from_argv(argv)
