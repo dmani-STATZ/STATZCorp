ea750e2e545bb6b390bb3d03690ef06a417901c3
dmani <dmani@statzcorp.com>
2025-04-03 12:40:31 -0500

    Add debug print statement for DJANGO_SETTINGS_MODULE in runserver_custom.py
    - Introduced a print statement in runserver_custom.py to output the value of DJANGO_SETTINGS_MODULE, aiding in debugging and ensuring correct environment variable access during server startup.

--- STATZWeb/management/commands/runserver_custom.py
+++ STATZWeb/management/commands/runserver_custom.py
@@ -6,4 +6,5 @@ class Command(RunserverCommand):
     def run_from_argv(self, argv):
         load_dotenv()
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE'))
+        print(f"DJANGO_SETTINGS_MODULE: {os.getenv('DJANGO_SETTINGS_MODULE')}")
         super().run_from_argv(argv)
