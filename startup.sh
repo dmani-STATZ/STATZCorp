20e8853c339b05cf2eb437fdf6d0fdb233adb9b8
Dion Mani <dmani@statzcorp.com>
2025-10-22 10:54:49 -0500

    Add build info setup to startup script and improve Azure DB connection tests
    - Added a command to set build information automatically during the startup process.
    - Enhanced the Azure database connection test by skipping the test if the SQL Server driver is not configured, improving test reliability.
    - Cleaned up the test output for better clarity on connection status.

--- startup.sh
+++ startup.sh
@@ -9,5 +9,8 @@ python manage.py collectstatic --noinput || true
 echo "Running Migrations..."
 python manage.py migrate || true
 
+echo "Setting Build Info..."
+python manage.py set_build_info --auto || true
+
 echo "Running Gunicorn..."
 gunicorn STATZWeb.wsgi:application --bind 0.0.0.0:$PORT --workers 3
